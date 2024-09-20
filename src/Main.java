59c4b5ccd2253c73405eb4aa785774556bc74de8
Chewbacca <higgimat000@warren.k12.in.us>
2024-09-20 13:35:59 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+    }
+
+    public static void printFactors(int number) {
+        // Check if number is negative. If so return -1
+        if (number < 0) {
+            System.out.println("Invalid value");
+        } else {
+            for (int i = 1; i <= number; i++) {
+                // check if number mod i is 0
+                // If so print it use System.out.print() not System.out.println().  Don't forget a space between
+                // If not don't do anything
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+        }
+
+
+    }
+
 }
